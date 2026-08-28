10381d693dea4bee6303ea02610240e6c68e6671
Banika <banika@amaranth.red>
2026-08-28 23:11:15 +0200

    End conversion if skin slinks amputated

--- Effect/AmalgamConversion.cs
+++ Effect/AmalgamConversion.cs
@@ -32,14 +32,50 @@ namespace XRL.World.Effects
                 "Putrified vitae has compromised your molecular structure, slowly transforming you into an amalgam.";
         }
 
+        public override bool Apply(GameObject Object)
+        {
+            if (Object.TryGetEffect(out AmalgamConversion fx))
+            {
+                if (!SkinSlink && fx.SkinSlink) fx.SkinSlink = false; // non-slink variant harder to remove, prefer that
+                return false;
+            }
+
+            return base.Apply(Object);
+        }
+
+        public bool CheckRemove()
+        {
+            // end effect if no more slinks on body
+            if (SkinSlink && !Object.HasBodyPart("Skin Slink"))
+            {
+                Object.RemoveEffect(this);
+                return true;
+            }
+
+            return false;
+        }
+
         public override bool WantEvent(int ID, int cascade)
         {
             return base.WantEvent(ID, cascade)
-                   || ID == EndTurnEvent.ID;
+                   || ID == EndTurnEvent.ID
+                   || ID == RegenerateDefaultEquipmentEvent.ID
+                   ;
+        }
+
+        public override bool HandleEvent(RegenerateDefaultEquipmentEvent E)
+        {
+            CheckRemove();
+            return base.HandleEvent(E);
         }
 
         public override bool HandleEvent(EndTurnEvent E)
         {
+            if (CheckRemove())
+            {
+                return true;
+            }
+
             if (Object == The.Player)
                 --CycleDuration;
             else
